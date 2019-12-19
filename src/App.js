5cae425e848540c06d9cb0ba18e837d664dc0cfb
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-19 08:56:30 +0200

    add calculator component and button for all numbers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Calculator from './calculator'
 
 function App() {
   return (
     <div className="App">
-      <h1>שלום עולם</h1>
+      <Calculator />
     </div>
   );
 }

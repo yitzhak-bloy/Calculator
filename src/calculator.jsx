a31f9008d953ab81262168cba9c8f2b3fdfe9f8a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-25 14:57:06 +0200

    Adds the option to see the digits I clicked on

--- src/calculator.jsx
+++ src/calculator.jsx
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Calculator() {
-    return (
-      <div className="Calculator">
-        <h1>מחשבון</h1>
-        <button id="equals">=</button>
-        <button id="zero">0</button>
-        <button id="one">1</button>
-        <button id="two">2</button>
-        <button id="three">3</button>
-        <button id="four">4</button>
-        <button id="five">5</button>
-        <button id="six">6</button>
-        <button id="seven">7</button>
-        <button id="eight">8</button>
-        <button id="nine">9</button>
-        <button id="add">+</button>
-        <button id="subtract">-</button>
-        <button id="multiply">*</button>
-        <button id="divide">/</button>
-        <button id="decimal">.</button>
-        <button id="clear">clear</button>
-      </div>
-    );
-  }
+
+  const [sumInDisplay, setsumInDisplay] = useState([0]);
+
+  return (
+    <div className="Calculator">
+      <h1>מחשבון</h1>
+      <button id="equals">=</button>
+      <button id="zero" onClick={() => setsumInDisplay([...sumInDisplay, 0])} >0</button>
+      <button id="one" onClick={() => setsumInDisplay([...sumInDisplay, 1])} >1</button>
+      <button id="two" onClick={() => setsumInDisplay([...sumInDisplay, 2])} >2</button>
+      <button id="three" onClick={() => setsumInDisplay([...sumInDisplay, 3])} >3</button>
+      <button id="four" onClick={() => setsumInDisplay([...sumInDisplay, 4])} >4</button>
+      <button id="five" onClick={() => setsumInDisplay([...sumInDisplay, 5])} >5</button>
+      <button id="six" onClick={() => setsumInDisplay([...sumInDisplay, 6])} >6</button>
+      <button id="seven" onClick={() => setsumInDisplay([...sumInDisplay, 7])} >7</button>
+      <button id="eight" onClick={() => setsumInDisplay([...sumInDisplay, 8])} >8</button>
+      <button id="nine" onClick={() => setsumInDisplay([...sumInDisplay, 9])} >9</button>
+      <button id="add" onClick={() => setsumInDisplay([...sumInDisplay, '+'])} >+</button>
+      <button id="subtract" onClick={() => setsumInDisplay([...sumInDisplay, '-'])} >-</button>
+      <button id="multiply" onClick={() => setsumInDisplay([...sumInDisplay, '*'])} >*</button>
+      <button id="divide" onClick={() => setsumInDisplay([...sumInDisplay, '/'])} >/</button>
+      <button id="decimal" onClick={() => setsumInDisplay([...sumInDisplay, '.'])} >.</button>
+      <button id="clear" onClick={() => setsumInDisplay([0])} >clear</button>
+      <div id='display'>{sumInDisplay}</div>
+    </div>
+  );
+}
   
-  export default Calculator;
+export default Calculator;

5bc54718e69de25a00e31da8466f4e969c2cf663
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-01-13 20:56:24 +0200

    Updates

---
+++ precache-manifest.460a4724177cead2bc0efaa9431713e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79949d4fcc832f12303d6b9bef80b4ec",
+    "url": "/yitzhak-bloy/calculator/index.html"
+  },
+  {
+    "revision": "c4a2d2daf3c35dd330a5",
+    "url": "/yitzhak-bloy/calculator/static/css/main.2eef2f19.chunk.css"
+  },
+  {
+    "revision": "ef2013c1c6ed6e4a60d9",
+    "url": "/yitzhak-bloy/calculator/static/js/2.47ee50ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yitzhak-bloy/calculator/static/js/2.47ee50ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4a2d2daf3c35dd330a5",
+    "url": "/yitzhak-bloy/calculator/static/js/main.f03d02f2.chunk.js"
+  },
+  {
+    "revision": "1873a3238c05cf2406cb",
+    "url": "/yitzhak-bloy/calculator/static/js/runtime-main.ff4dd297.js"
+  },
+  {
+    "revision": "ad2511e4d88e431b95fd6f121f928b0f",
+    "url": "/yitzhak-bloy/calculator/static/media/dustin-lee-jLwVAUtLOAQ-unsplash.ad2511e4.jpg"
+  }
+]);

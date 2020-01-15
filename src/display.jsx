97d24b19259951f9f9ecac652c8ee17ef161efe5
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-01-15 10:26:31 +0200

    Small correction

--- src/display.jsx
+++ src/display.jsx
@@ -5,7 +5,7 @@ import './display.css';
 const Display = ({count, sum}) => (
   <div id='display'>
     <div id='displayCount'>{count ? count : '0'}</div>
-    <div id='displaySum'></div>{sum}
+    <div id='displaySum'>{sum}</div>
   </div>
 );
 

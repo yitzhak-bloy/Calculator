6ed53c72bce3803e2127260e4ddd9dc5b7410c6f
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-03-10 21:53:08 +0200

    displayNumbers

--- src/display.jsx
+++ src/display.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './display.css';
 
 const Display = ({count, sum}) => (
-  <div id='display'>
+  <div id='displayNumbers'>
     <div id='displayCount'>{count ? count : '0'}</div>
     <div id='displaySum'>{sum}</div>
   </div>

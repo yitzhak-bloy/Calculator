9c57c862a5188141163e4d3d03e758f6815fa7ec
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-03-25 11:22:31 +0200

    Small changes to make the app better deployed

--- src/display.jsx
+++ src/display.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './display.css';
 
-const Display = ({count, sum}) => (
-  <div id='displayNumbers'>
+const Display = ({ count, sum }) => (
+  <div className='displayNumbers'>
     <div id='displayCount'>{count ? count : '0'}</div>
     <div id='displaySum'>{sum}</div>
   </div>
